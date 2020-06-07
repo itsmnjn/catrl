18a8c650a16317f27d9ea46bac13547bc1c00c8f
Min Jun Kim <mk7pe@virginia.edu>
2020-06-07 16:29:28 -0400

    Add adjectives.ts and felines.ts

---
+++ felines.ts
@@ -0,0 +1,23 @@
+const felines: string[] = [
+  'cat',
+  'leopard',
+  'bobcat',
+  'cougar',
+  'jaguar',
+  'kitten',
+  'kitty',
+  'lion',
+  'lynx',
+  'ocelot',
+  'panther',
+  'puma',
+  'tabby',
+  'tiger',
+  'tom',
+  'tomcat',
+  'minmin',
+  'bonbon',
+  'wonku',
+];
+
+export default felines;
